b3ab1a52bf363f61b265447f8d177392245e08de
Jacob_Moxley <jacobcmoxley@gmail.com>
2022-07-04 13:40:36 -0600

    working on loopvectorization for 1d bit model

--- src/IsingModel.jl
+++ src/IsingModel.jl
@@ -1,4 +1,4 @@
-using LinearAlgebra, IfElse
+using LinearAlgebra, IfElse, Random
 
 module IsingModel
 
@@ -22,13 +22,50 @@ function SerialStepInt!(m::IsingModelInt, Cells::Tuple{Int}, temp::Array{Int8,1}
     h_min = -2
     h_max = 2
     prob = [1/(1+exp(-2*m.β*h)) for h ∈ h_min:h_max]
+    n = length(m.State)
     @tturbo for i ∈ eachindex(m.State)
         #left = s[i == 1 ? n : i-1]
         #right = s[i == n ? 1 : i+1]
         h = m.State[i == 1 ? n : i-1] + m.State[i == n ? 1 : i+1]
-        si_old = m.State[i]
-        m.State[i] = rand(Float64) < prob[h-h_min+1] ? 1 : -1
+        #si_old = m.State[i]
+        temp[i] = rand(Float64) < prob[h-h_min+1] ? 1 : -1
     end
+    return temp
+end
+
+function 1dBitLog(A)
+    n = length(A)
+    for i ∈ eachindex(A)
+        left = A[i==1 ? n : i-1]
+        right = A[i==n ? 1 : i+1]
+        if (left && right)
+            h = 1
+        elseif (left ⊻ right)
+            h = 2
+        else
+            h = 3
+        end
+    end
+end
+
+function SerialStepBit!(m::IsingModelBit, Cells::Tuple{Int}, temp::BitArray{1})
+    h_min = -2
+    h_max = 2
+    prob = [1/(1+exp(-2*m.β*h)) for h ∈ h_min:2:h_max]
+    n = length(m.State)
+    for i ∈ eachindex(m.State)
+        left = m.State[i==1 ? n : i-1]
+        right = m.State[i==n ? 1 : i+1]
+        if left & right
+            h = 1
+        elseif left ⊻ right
+            h = 2
+        else
+            h = 3
+        end
+        temp[i] = rand(Float64) < prob[h] ? 1 : 0
+    end
+    return temp
 end
 
 function EvaluateModel(m::IsingModel)
