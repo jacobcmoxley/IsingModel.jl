adc4fd4d7d62cc319d848be0849d436f8dde593a
Jacob_Moxley <jacobcmoxley@gmail.com>
2022-07-04 10:53:35 -0600

    working on two different versions for ints and bits at this point

--- src/IsingModel.jl
+++ src/IsingModel.jl
@@ -1,5 +1,38 @@
+using LinearAlgebra, IfElse
+
 module IsingModel
 
-greet() = print("Hello World!")
+mutable struct IsingModelInt
+    Cells::Tuple{Vararg{Int,N} where N}
+    Steps::Int
+    #SaveStep::Int
+    State::Array{Int8}
+    β::Float64
+end
+
+mutable struct IsingModelBit
+    Cells::Tuple{Vararg{Int,N} where N}
+    Steps::Int
+    #SaveStep::Int
+    State::BitArray
+    β::Float64
+end
+
+function SerialStepInt!(m::IsingModelInt, Cells::Tuple{Int}, temp::Array{Int8,1})
+    h_min = -2
+    h_max = 2
+    prob = [1/(1+exp(-2*m.β*h)) for h ∈ h_min:h_max]
+    @tturbo for i ∈ eachindex(m.State)
+        #left = s[i == 1 ? n : i-1]
+        #right = s[i == n ? 1 : i+1]
+        h = m.State[i == 1 ? n : i-1] + m.State[i == n ? 1 : i+1]
+        si_old = m.State[i]
+        m.State[i] = rand(Float64) < prob[h-h_min+1] ? 1 : -1
+    end
+end
+
+function EvaluateModel(m::IsingModel)
+    return nothing
+end
 
 end # module
