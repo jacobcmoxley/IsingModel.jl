d13c8f9897b9a0974f06ca69d0414f08720f2f68
Jacob_Moxley <jacobcmoxley@gmail.com>
2022-07-05 10:04:30 -0600

    2d serial step working

--- src/IsingModel.jl
+++ src/IsingModel.jl
@@ -23,56 +23,26 @@ function SerialStep!(m::Ising, Cells::Tuple{Int}, temp::Array{Int8,1})
         #left = s[i == 1 ? n : i-1]
         #right = s[i == n ? 1 : i+1]
         h = m.State[i == 1 ? n : i-1] + m.State[i == n ? 1 : i+1]
-        #si_old = m.State[i]
         temp[i] = rand(Float64) < prob[h-h_min+1] ? 1 : -1
     end
     return temp
 end
 
-#=
-mutable struct IsingModelBit
-    Cells::Tuple{Vararg{Int,N} where N}
-    Steps::Int
-    SaveStep::Int
-    State::BitArray
-    β::Float64
-end
-
-function 1dBitLog(A)
-    n = length(A)
-    for i ∈ eachindex(A)
-        left = A[i==1 ? n : i-1]
-        right = A[i==n ? 1 : i+1]
-        if (left && right)
-            h = 1
-        elseif (left ⊻ right)
-            h = 2
-        else
-            h = 3
-        end
-    end
-end
-
-function SerialStepBit!(m::IsingModelBit, Cells::Tuple{Int}, temp::BitArray{1})
-    h_min = -2
-    h_max = 2
-    prob = [1/(1+exp(-2*m.β*h)) for h ∈ h_min:2:h_max]
+function SerialStep!(m::Ising, Cells::Tuple{Int}, temp::Array{Int8,2})
+    h_min = -4
+    h_max = 4
+    prob = [1/(1+exp(-2*m.β*h)) for h ∈ h_min:h_max]
     n = length(m.State)
-    @inbounds @simd for i ∈ eachindex(m.State)
-        left = m.State[i==1 ? n : i-1]
-        right = m.State[i==n ? 1 : i+1]
-        if left & right
-            h = 1
-        elseif left ⊻ right
-            h = 2
-        else
-            h = 3
-        end
-        temp[i] = rand(Float64) < prob[h] ? 1 : 0
+    @inbounds for j in 1:n, i in 1:m
+        top = m.State[i == 1 ? m : i-1, j]
+        bottom = m.State[i == m ? 1 : i+1, j]
+        right = m.State[i, j == 1 ? n : j-1]
+        left = m.State[i, j == n ? 1 : j+1]
+        h = top + bottom + right + left
+        temp[i,j] = rand(Float64) < prob[h-min_h+1] ? +1 : -1
     end
     return temp
 end
-=#
 
 function EvaluateModel!(m::Ising, StepFunction::Function)
     f = open(m.SaveFile,"w")
